1b85e8be05644d368d0493428a56f8150d113362
Amos Johnson <63177847+AmosJAMcLovin@users.noreply.github.com>
2023-04-20 15:10:48 -0400

    Delete connectvars.php

--- connectvars.php
+++
@@ -1,7 +0,0 @@
-<?php
-  // Define the database connection constants here!
- define('DB_HOST', 'localhost');
- define('DB_USER', 'w23afc');     // your scweb username
- define('DB_PASSWORD', 'r8trxr8trxv12tgv12tg');  // See blackboard for 20-char password
- define('DB_NAME', 'w23afcdb');   // username followed by lowercase db
-?>
